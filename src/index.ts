4f92b0c22ef313bb0bf5a97bd4bd157e6eb15cbc
Vlad Balin <gaperton@gmail.com>
2017-04-21 19:04:06 -0400

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import * as React from 'react'
-import { define, Record, Store, mixins, mergeProps, extendable, mixinRules, tools, Mixable, MixinRules } from 'type-r'
+import { define, Record, Store, mixins, mixinRules, ChainableAttributeSpec } from 'type-r'
 import processSpec, { Node, Element, TypeSpecs } from './define'
-import Link from './Link'
+import Link from './link'
 import { Component, createClass } from './component'
 
+interface ReactMVx{
+    // It's ES6 module
+    default : ReactMVx
+    
+    // MixtureJS decorators...
+    define : typeof define
+    mixins : typeof mixins
+    mixinRules : typeof mixinRules
+
+    // Overriden components
+    createClass : typeof createClass
+    Component : typeof Component
+
+    // additional ReactMVx types
+    Link : typeof Link
+    Node : ChainableAttributeSpec
+    Element : ChainableAttributeSpec
+
+    // Helper methods
+    assignToState : typeof Component.prototype.assignToState
+}
+
 // extend React namespace
-const ReactMVx = Object.create( React );
+const ReactMVx : ReactMVx = Object.create( React );
+
 // Make it compatible with ES6 module format.
 ReactMVx.default = ReactMVx;
 export default ReactMVx;
